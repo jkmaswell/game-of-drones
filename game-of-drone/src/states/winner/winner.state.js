2bb6035b49e383b14cc26506c2c2dbaa1754a690
Juan Camilo Isaza <juancamilo.isaza@wolox.co>
2019-02-25 07:59:32 -0500

    fixing responsiveness

--- game-of-drone/src/states/winner/winner.state.js
+++ game-of-drone/src/states/winner/winner.state.js
@@ -23,17 +23,13 @@ export class WinnerComponent extends Component {
 
   render() {
     return (
-      <div className="winner">
-        <header>
+      <section>
+        <div className="winner">
           <h1>We have a winner</h1>
-        </header>
-        <main>
-          <section>
-            <span className="disc"><span className="winner-name">{this.state.winner}</span> is the new EMPEROR!</span>
-            <button id="go-home" type="button" onClick={this.goToHome}>Play again</button>
-          </section>
-        </main>
-      </div>
+          <span className="disc"><span className="winner-name">{this.state.winner}</span> is the new EMPEROR!</span>
+          <button id="go-home" type="button" onClick={this.goToHome}>Play again</button>
+        </div>
+      </section>
     );
   }
 }
